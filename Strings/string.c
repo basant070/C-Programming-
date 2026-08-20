cac56fbdcabdeb2b741408f338d4eea4e23d2c67
Basant Bhat <basantbhat70@gmail.com>
2026-08-20 22:19:15 +0545

    first string

---
+++ Strings/string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char str[30];
+    printf("Enter your University name :");
+    gets(str);
+    // scanf("%s", &str);
+    // printf("%s", str);
+    puts(str);
+    return 0;
+}
